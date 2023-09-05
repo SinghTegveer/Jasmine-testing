d5ea9c58c559796fc526b217b4b3a1033dea92c1
SinghTegveer <tegveersingh9271@gmail.com>
2023-09-05 02:13:48 -0400

    Adding functional Calculator application

--- calculator.js
+++ calculator.js
@@ -1,15 +1,25 @@
+console.log("Calculator Script invoked");
 function Calculator() {
+    console.log("Constructor method calculator called");
     this.total = 0;
 }
-Calculator.prototype.add = (num) => {
-    return this.total += num
+// Cannot use arrow functions here since arrow functions do not have their own this object
+Calculator.prototype.add = function (num) {
+    this.total += num;
+    return this.total;
 }
-Calculator.prototype.subtract = (num) => {
-    return this.total -= num
+Calculator.prototype.subtract = function (num) {
+    this.total -= num;;
+    return this.total;
 }
-Calculator.prototype.multiply = (num) => {
-    return this.total *= num
+Calculator.prototype.multiply = function (num) {
+    this.total *= num
+    return this.total;
 }
-Calculator.prototype.divide = (num) => {
-    return this.total /= num
+Calculator.prototype.divide = function (num) {
+    if (num === 0) {
+        throw new Error("Denominator cannot be 0")
+    }
+    this.total /= num;
+    return this.total;
 }
---
+++ main.js
@@ -0,0 +1,53 @@
+console.log("Main Script invoked");
+
+// Method to update the span display for calculator output
+function updateResult(result) {
+    console.log("Update result method called");
+    // Can alternatively use inner text or inner HTML
+    document.getElementById("calculatorOutput").innerHTML = result
+}
+
+// Event handler to handle input field change
+document.getElementById('calculatorInput').addEventListener('change', (event) => {
+    console.log("Event: ", event);
+    console.log("Input value: ", event.target.value);
+
+    const value = event.target.value;
+    // Use regular expression to get values of first and second number and the operator
+    const reg = /\+|\*|\-|\//
+    // Use split method to separate based on regex - regex101 website
+    const inputSplit = value.split(reg);
+    console.log("Input Split: ", inputSplit);
+    const num1 = parseInt(inputSplit[0]);
+    const num2 = parseInt(inputSplit[1]);
+    // Use match method to return the regex match
+    const operator = value.match(reg)[0];
+
+    console.log("A: ", num1, " B: ", num2);
+    console.log("Operator: ", operator);
+
+    // Initialize calculator object - creates a new object everytime same as doing num1 + num2 rather than only having one number adding and subtracting later
+    var cal = new Calculator();
+    cal.add(num1)
+
+    switch (operator) {
+        case "+":
+            result = cal.add(num2);
+            break;
+        case "-":
+            result = cal.subtract(num2);
+            break;
+        case "*":
+            result = cal.multiply(num2);
+            break;
+        case "/":
+            result = cal.divide(num2);
+            break;
+        default:
+            console.log("Invalid operator");
+            throw new Error("Invalid operation requested")
+    }
+    // Display the result and map to the HTML screen
+    console.log("Result: ", result);
+    updateResult(result);
+})
